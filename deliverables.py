a92f62768e29d93ae915866df8772d42b7e44f4f
ybhagwan <99209398+ybhagwan@users.noreply.github.com>
2022-06-29 11:01:08 +0530

    Update deliverables.py

--- deliverables.py
+++ deliverables.py
@@ -1,45 +1,90 @@
-#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 from threading import Thread
 import sys
-import yaml
+from ruamel.yaml import YAML
 import os
 import time
 from datetime import datetime
+import subprocess
+#python3 deliverables.ya "build cluster" "run cluster"  "workloadType" "n-workloads" "dev-namespace"
 
-#python3 deliverables.ya "build cluster" "run cluster" "n-workloads" "dev-namespace"
-
+yaml = YAML()
+yaml.preserve_quotes = True
 timestamp1 = datetime.now()
 timestamp = timestamp1.strftime('%d-%b-%Y-%H-%M-%S')
 dir1 = os.getcwd() + 'deliverables-'+timestamp
 os. makedirs(dir1)
 buildCluster = sys.argv[1]
 runCluster = sys.argv[2]
-workloads = int(sys.argv[3])
-nameSpace = sys.argv[4]
+workloadType = sys.argv[3]
+workloads = int(sys.argv[4])
+nameSpace = sys.argv[5]
 
-os.system('kubectl config use-context '+ buildCluster)
-print("contexts set to build cluster")
-time.sleep(5)
-for i in range(1,workloads+1):
-    os.system('kubectl get deliverable spring-petclinic-'+str(i)+' --namespace '+nameSpace+' -oyaml > deliverables-spring-petclinic-'+str(i)+'.yaml')
-    with open(dir1+'/deliverables-spring-petclinic-'+str(i)+'.yaml', 'w+') as stream:
+ExecStartTime = datetime.now()
+print("Start time : "+str(ExecStartTime))
+
+def createDeliverables(buildCluster, workloadType, nWorkloads, nameSpace):
+    proc1 = subprocess.Popen('kubectl config use-context '+ buildCluster,shell = True, stdout=subprocess.PIPE)
+    result,err = proc1.communicate()
+    print(result.decode('ascii'))
+    time.sleep(5)
+    #deliverableName default name
+    deliverableName = "tanzu-java-web-app-"
+    if(workloadType == "medium"):
+        deliverableName = "spring-petclinic-"
+
+    for i in range(1,nWorkloads+1):
+        proc1 = subprocess.Popen('kubectl get deliverable'+deliverableName+str(i)+' --namespace '+nameSpace+' -oyaml > deliverables-'+deliverableName+str(i)+'.yaml')
+        result,err = proc1.communicate()
+        print(result.decode('ascii'))
+        with open(dir1+'/deliverables-'+deliverableName+str(i)+'.yaml', 'w+') as stream:
+            try:
+                d=yaml.safe_load(stream)
+                del(d['status'])
+                del(d['metadata']['ownerReferences'])
+                stream.flush()
+                yaml.dump(d, stream, allow_unicode=True)
+            except yaml.YAMLError as e:
+                print(e)
+
+def importDeliverables(runCluster, workloadType, nWorkloads, nameSpace):
+    proc1 = subprocess.Popen('kubectl config use-context '+ runCluster,shell = True, stdout=subprocess.PIPE)
+    result,err = proc1.communicate()
+    print(result.decode('ascii'))
+    time.sleep(5)
+    #defining default name
+    deliverableName = "tanzu-java-web-app-"
+    if(workloadType == "medium"):
+        deliverableName = "spring-petclinic-"
+
+    for i in range(1,workloads+1):
+        proc1 = subprocess.Popen('kubectl apply -f '+dir1+'/deliverables-'+deliverableName+str(i)+'.yaml -n '+nameSpace+' &')
+        result,err = proc1.communicate()
+        print(result.decode('ascii'))
+
+def checkWorkloadsStatus(nworkloads, nameSpace, period):
+    timeLapse = 0
+    startTime = time.time()
+    while(timeLapse <= period):
         try:
-            print(stream)
-            d=yaml.safe_load(stream)
-            del(d['status'])
-            del(d['metadata']['ownerReferences'])
-            stream.flush()
-            yaml.dump(d, stream, allow_unicode=True)
-        except yaml.YAMLError as e:
+            proc = subprocess.Popen("kubectl get ksvc -n "+nameSpace,shell = True, stdout=subprocess.PIPE)
+            result,err = proc.communicate()
+            ksvcStaus = result.decode('ascii').count("True")
+            print("Total number of workloads have ksvc in True state : "+str(ksvcStaus))
+            print("Total number of workloads have ksvc in False state: "+str(nworkloads - ksvcStaus))
+            print("Total time elapsed in seconds     : "+str(timeLapse))
+            if(ksvcStaus == nworkloads):
+                print("All workloads ksvc are in True state in "+str(timeLapse)+" sec")
+                break
+            time.sleep(3)
+            timeLapse = int(time.time() - startTime)
+        except UnicodeDecodeError as e:
             print(e)
 
-print('Switching to run cluster from build cluster')
-os.system('kubectl config use-context '+runCluster)
-os.system('kubectl config get-contexts')
-print('Successfully switched to run cluster')
-for i in range(1,workloads+1):
-    os.system('kubectl apply -f '+dir1+'/deliverables-spring-petclinic-'+str(i)+'.yaml -n '+nameSpace+' &')
+createDeliverables()
+importDeliverables()
+checkWorkloadsStatus()
 
-print('waiting for 30 sec after importing build cluster deliverables in run cluster')
-time.sleep(30)
-os.system('kubectl get deliverables -A')
+ExecEndTime = datetime.now()
+print("===========================")
+print("Execution Start Time : "+str(ExecStartTime))
+print("Execution End Time   : "+str(ExecEndTime))
